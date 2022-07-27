bfdf2ed1455d5238d9163d1cdb8b022858612de7
lreboseno22 <liamrebo22@gmail.com>
2022-07-27 14:14:44 -0400

    Route to create player and authentication set

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const authCheck = require('./middleware/checkAuth');
+const { generateToken } = require('./utils');
+
+const { pool } = require('./db');
 
 const app = express();
 const PORT = 3032;
@@ -7,10 +12,75 @@ const PORT = 3032;
 app.use(express.json());
 app.use(cors());
 
-app.get('/', async(req, res) => {
-    console.log("hello")
+/* 
+Route to add a new user to our players table
+Requirements for the body to use: 
+- username (less than 20 characters long)
+- password (password chosen to sign in with this account)
+*/
+app.post('/signup', async(req, res) => {
+    const playerUsername = req.body.username;
+    const playerPassword = req.body.password;
+    const saltRounds = 10 // Required for authentication
+    try {
+        const hashedPassword = await bcrypt.hash(playerPassword, saltRounds);
+        const sql = `INSERT INTO players (username, password, pfp_link, games_won, games_lost) VALUES ($1, $2, $3, $4, $5) returning *;`
+        const databaseResult = await pool.query(sql, [playerUsername, hashedPassword, null, 0, 0]);
+        console.log(databaseResult);
+        const playerToken = generateToken(databaseResult.rows[0].player_id);
+        res.status(201).json({
+            newPlayer: databaseResult.rows[0],
+            token: playerToken
+        })
+    } catch(error){
+        res.status(500).json({ message: `${error.message }` });
+    }
+});
+
+/*
+Route to login
+Requirements for the body to use:
+- username -> used to query the database for the hashed password
+- password -> used when signing up to test against decrypted hashed password in DB
+*/
+app.post('/login', async(req, res) => {
+    try{
+        const { username, password } = req.body;
+        const sql = `SELECT * from players where username $1`
+        const databaseResult = await pool.query(sql, [username]);
+        if(!databaseResult.rows[0]){
+            return res.status(401).json({
+                message: "You sure you have the right username?",
+            });
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, databaseResult.rows[0].password)
+        if(!isPasswordCorrect){
+            return res.status(401).json({
+                message: "You sure you have the right password?",
+            });
+        }
+        const token = generateToken();
+        return res.status(200).json({
+            playerInfo: databaseResult.rows[0],
+            token
+        });
+    } catch(error){
+        res.status(500).json({ message: `${error.message }` });
+    }
+});
+
+app.get('/players', async(req, res) => {
+    try {
+        const databaseResult = await pool.query(`SELECT * FROM players`)
+        console.log(databaseResult.rows);
+        res.json({
+            data: databaseResult.rows
+        });
+    } catch(error){
+        res.status(500).json({ message: `${error.message }` });
+    }
 })
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
-})
+});
